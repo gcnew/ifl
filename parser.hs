bf48fb5b9f5b5f0a36618abfeb4a4cf1c0d06604
Marin Marinov <marin@chobolabs.com>
2016-01-04 01:40:02 +0200

    Progress..

--- parser.hs
+++ parser.hs
@@ -57,23 +57,73 @@ preludeDefs = [
         EAp (EVar "f") (EAp (EVar "g") (EVar "x"))),
     ("twice", ["f"], EAp (EAp (EVar "compose") (EVar "f")) (EVar "f")) ]
 
-iNil     :: Iseq                  -- The empty iseq
-iStr     :: String -> Iseq        -- Turn a string into an iseq
-iAppend  :: Iseq -> Iseq -> Iseq  -- Append two iseqs
-iNewline :: Iseq                  -- New line with indentation
-iIndent  :: Iseq -> Iseq          -- Indent an iseq
-iDisplay :: Iseq -> String        -- Turn an iseq into a string
+data Iseq = INil
+    | IStr String
+    | IAppend Iseq Iseq
+
+iNil :: Iseq                     -- The empty iseq
+iNil = INil
+
+iStr :: String -> Iseq           -- Turn a string into an iseq
+iStr str = IStr str
+
+iAppend :: Iseq -> Iseq -> Iseq  -- Append two iseqs
+iAppend s1 s2 = IAppend s1 s2
+
+iNewline :: Iseq                 -- New line with indentation
+iNewline = IStr "\n"
+
+iIndent :: Iseq -> Iseq          -- Indent an iseq
+iIndent s = s
+
+iDisplay :: Iseq -> String       -- Turn an iseq into a string
+iDisplay s = flatten [s]
+
+iConcat :: [Iseq] -> Iseq
+iConcat xs = foldl iAppend iNil xs
+
+iInterleave :: Iseq -> [Iseq] -> Iseq
+iInterleave sep seqs = iConcat list
+    where list :: [Iseq]
+          list = join sep seqs
+
+          join :: Iseq -> [Iseq] -> [Iseq]
+          join _ []     = []
+          join _ (x:[]) = [x]
+          join s (x:xs) = (x : s : (join s xs))
+
+flatten :: [Iseq] -> String
+flatten []                     = ""
+flatten (INil : seqs)          = flatten seqs
+flatten (IStr s : seqs)        = s ++ flatten seqs
+flatten (IAppend s1 s2 : seqs) = flatten (s1 : s2 : seqs)
 
 pprExpr :: CoreExpr -> Iseq
 pprExpr (EVar v)    = iStr v
+pprExpr (ENum num)  = iStr (show num)
 pprExpr (EAp e1 e2) = (pprExpr e1) `iAppend` (iStr " ") `iAppend` (pprAExpr e2)
+
+pprExpr (EConstr tag arity)
+    = iConcat [ iStr "Pack{",
+                iStr (show tag), iStr ", ",
+                iStr (show arity), iStr "}" ]
 
 pprExpr (ELet isrec defns expr)
     = iConcat [ iStr keyword, iNewline,
                 iStr "  ", iIndent (pprDefns defns), iNewline,
                 iStr "in ", pprExpr expr ]
-    where keyword | not isrec = "let"
-                  | isrec = "letrec"
+    where keyword | isrec     = "letrec"
+                  | otherwise = "let"
+
+pprExpr (ELam vars expr) = iConcat [ iStr "\\ ", (pprVarNames vars), iStr ". ", pprExpr expr ]
+
+pprExpr (ECase expr alts)
+    = iConcat [ iStr "case ", pprExpr expr ]
+
+pprAExpr :: CoreExpr -> Iseq
+pprAExpr e
+    | isAtomicExpr e = pprExpr e
+    | otherwise      = (iStr "(") `iAppend` pprExpr e `iAppend` (iStr ")")
 
 pprDefns :: [(Name,CoreExpr)] -> Iseq
 pprDefns defns = iInterleave sep (map pprDefn defns)
@@ -82,16 +132,25 @@ pprDefns defns = iInterleave sep (map pprDefn defns)
 pprDefn :: (Name, CoreExpr) -> Iseq
 pprDefn (name, expr) = iConcat [ iStr name, iStr " = ", iIndent (pprExpr expr) ]
 
-iConcat :: [Iseq] -> Iseq
-iConcat xs = foldl iAppend iNil xs
+pprProgram :: CoreProgram -> Iseq
+pprProgram prog = iInterleave (iStr "\n\n") (map pprScDefn prog)
 
-iInterleave :: Iseq -> [Iseq] -> Iseq
-iInterleave sep xs = iConcat list
-    where list = join sep xs
-          join s []     = []
-          join s (x:xs) = (x : s : (join s xs))
+pprScDefn :: CoreScDefn -> Iseq
+pprScDefn (name, vars, expr) = iConcat [ iStr name, iStr " ", pprVarNames vars, iStr " = ", pprExpr expr ]
 
-main = show preludeDefs
+pprVarNames :: [String] -> Iseq
+pprVarNames names = iInterleave (iStr " ") (map iStr names)
+
+pprint :: CoreProgram -> String
+pprint prog = iDisplay (pprProgram prog)
+
+
+mkMultiAp :: Int -> CoreExpr -> CoreExpr -> CoreExpr
+mkMultiAp n e1 e2 = foldl EAp e1 (take n e2s)
+    where e2s = e2 : e2s
+
+main :: IO ()
+main = putStrLn $ pprint preludeDefs
 
 
 
